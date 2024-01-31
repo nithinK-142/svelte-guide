5363447b36485f842bc5b889cf15206c06d3d84c
nithinK-142 <nithin.sagar.359@gmail.com>
2024-01-31 14:26:39 +0530

    feat: auto subscriptions.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
 	import { counter } from '$lib/counter';
 
-	let count = 0;
-
-	counter.subscribe((value: number) => {
-		count = value;
-	});
-
-	const increment = () => counter.update((count: number) => count + 1);
-	const decrement = () => counter.update((count: number) => count - 1);
-	const reset = () => counter.set(0);
+	const increment = () => ($counter += 1);
+	const decrement = () => ($counter -= 1);
+	const reset = () => ($counter = 0);
 </script>
 
 <h1 class="pt-10 text-3xl text-center text-white/90">Welcome to SvelteKit Guide</h1>
 
 <div class="flex flex-col items-center text-white">
-	<h2 class="pt-10 text-5xl">Count is {count}</h2>
+	<h2 class="pt-10 text-5xl">Count is {$counter}</h2>
 	<div class="flex gap-6 mt-4">
 		<button on:click={decrement} class="px-3 pb-1 text-4xl rounded-md bg-white/10"> - </button>
 		<button on:click={increment} class="px-3 pb-1 text-4xl rounded-md bg-white/10"> + </button>
